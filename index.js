4d961edfaeea018e3df64aab9812dadbdcf12a61
nicholas koyi <koyinicholas@gmail.com>
2024-03-23 09:15:48 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = "bob";
+var bestCustomer;
+const leastFavoriteCustomer = "Grace";
+
+function upperCaseCustomerName() {
+  customerName = "BOB";
+  return customerName;
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+  return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+  return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Brian";
+  return leastFavoriteCustomer;
+}
